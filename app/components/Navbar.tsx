18a7be7ef2fd3e7cd8673084e9c0937ffc9a675f
Jon Flores <jflore61@ucsc.edu>
2023-11-17 19:50:56 +0700

    Responsive Navbar with no mobile menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,78 @@
+"use client";
+
+// Import Next Components
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+// Import ShadcnUI Components
+import { Button } from "@/components/ui/button";
+
+// Import Lucide React Components
+import { ShoppingBag } from "lucide-react";
+
+// Create nav link array
+const links = [
+  { name: "Home", href: "/" },
+  { name: "Men", href: "/Men" },
+  { name: "Women", href: "/Women" },
+  { name: "Teens", href: "/Teens" },
+];
+
+// start of tsx
 export default function Navbar() {
+  {
+    /* create pathname variable with current url value using usePathname Hook */
+  }
+  const pathname = usePathname();
   return (
-    <div>
-      <h1>Hello from navbar</h1>
-    </div>
+    <header className="mb-8 border-b">
+      {/* Navbar Flex container */}
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        {/* Logo navButton to Home */}
+        <Link href="/">
+          <h1 className="text-2xl md:text-4xl font-bold">
+            Next<span className="text-primary">Commerce</span>
+          </h1>
+        </Link>
+
+        {/* Navbar Links to pages Flexbox */}
+        <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+          {/* Navbar Links to pages Map function */}
+          {links.map((link, idx) => (
+            <div key={idx}>
+              {/* If the current page the link is the primary color esle it is grey with a hover effect that turns it the primary color */}
+              {pathname === link.href ? (
+                <Link
+                  className="text-lg font-semibold text-primary"
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              ) : (
+                <Link
+                  href={link.href}
+                  className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary"
+                >
+                  {link.name}
+                </Link>
+              )}
+            </div>
+          ))}
+        </nav>
+
+        {/*Shoppping cart logo */}
+        <div className="flex divide-x border-r sm: border-l">
+          <Button
+            variant={"outline"}
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+          >
+            <ShoppingBag />
+            <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+              Cart
+            </span>
+          </Button>
+        </div>
+      </div>
+    </header>
   );
 }
